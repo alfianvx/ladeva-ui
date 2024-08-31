eaca36a03eba28895a395fa475a4bc6f389fadbe
alfianvx <devsroot8@gmail.com>
2024-08-31 07:56:12 +0700

    feat(app): add icon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="180x180"
+          href="/apple-icon?apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icon?favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icon?favicon-16x16.png"
+        />
+        <link rel="manifest" href="/icon/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
