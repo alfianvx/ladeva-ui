7f312e5b92ad2f9f6be11910dc157e4742effb2d
alfianvx <devsroot8@gmail.com>
2024-08-31 17:33:24 +0700

    feat: Improve FAQ section styling and add smooth scrolling

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -8,37 +8,39 @@ import {
 
 export default function Faq() {
   return (
-    <section className="flex flex-col px-5 py-10 mx-auto md:py-16 max-w-7xl md:px-0">
-      <h1 className="text-2xl font-semibold md:text-4xl">
-        What. The. F.A.Q<span className="text-ladeva">.</span>
-      </h1>
-      <p className="max-w-xl py-3 text-sm md:py-5 ">
-        Some questions that are often asked by old and new clients. if there is
-        something unclear please contact us, don&apos;t hesitate.{" "}
-        <span className="text-ladeva">ladeva@hay.com</span>
-      </p>
-      <div className="flex flex-wrap">
-        <Image
-          src={"/3d/speaker.svg"}
-          alt="spekaer-icon-3d"
-          className="w-10 md:basis-2/4 basis-full"
-          width={300}
-          height={300}
-        />
-        <Accordion
-          type="single"
-          className="md:basis-2/4 basis-full"
-          collapsible
-        >
-          {faqs.map((faq) => (
-            <AccordionItem key={faq.id} value={`item-${faq.id}`}>
-              <AccordionTrigger className="text-sm md:text-base">
-                {faq.question}
-              </AccordionTrigger>
-              <AccordionContent>{faq.answer}</AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
+    <section id="faq" className="bg-[#F1F5F9] scroll-m-20">
+      <div className="flex flex-col px-5 py-10 mx-auto md:pt-16 max-w-7xl md:px-0">
+        <h1 className="text-2xl font-semibold md:text-4xl">
+          What. The. F.A.Q<span className="text-ladeva">.</span>
+        </h1>
+        <p className="max-w-xl py-3 text-sm md:py-5 ">
+          Some questions that are often asked by old and new clients. if there
+          is something unclear please contact us, don&apos;t hesitate.{" "}
+          <span className="text-ladeva">ladeva@hay.com</span>
+        </p>
+        <div className="flex flex-wrap">
+          <Image
+            src={"/3d/speaker.svg"}
+            alt="spekaer-icon-3d"
+            className="w-10 md:basis-2/4 basis-full"
+            width={300}
+            height={300}
+          />
+          <Accordion
+            type="single"
+            className="md:basis-2/4 basis-full"
+            collapsible
+          >
+            {faqs.map((faq) => (
+              <AccordionItem key={faq.id} value={`item-${faq.id}`}>
+                <AccordionTrigger className="text-sm md:text-base">
+                  {faq.question}
+                </AccordionTrigger>
+                <AccordionContent>{faq.answer}</AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
       </div>
     </section>
   );
