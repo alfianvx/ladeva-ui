e07a446ecfb9c5d89782945f457aa37360c18b3d
alfianvx <devsroot8@gmail.com>
2024-08-31 17:36:55 +0700

    feat: update scroll behavior and add id to how-it-work section

--- src/components/workflow.tsx
+++ src/components/workflow.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 
 export default function Workflow() {
   return (
-    <section className="flex flex-col items-center justify-center px-5 mx-auto my-14 md:my-20 md:px-0 max-w-7xl">
+    <section
+      id="how-it-work"
+      className="flex flex-col items-center justify-center px-5 mx-auto my-14 md:my-20 md:px-0 max-w-7xl scroll-m-32"
+    >
       <h1 className="text-2xl font-semibold text-center md:text-4xl">
         How does it work<span className="text-ladeva">.</span>
       </h1>
