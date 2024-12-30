38dec5d81e22a2b63670ea9b7353b067584e36f4
alfianvx <gusjabungaf@gmail.com>
2024-12-30 17:17:29 +0700

    fix: add Cache-Control header to getTestimonials API request

--- src/service/Testimonial.ts
+++ src/service/Testimonial.ts
@@ -1,5 +1,11 @@
 import { http } from "@/lib/http";
 
 export const getTestimonials = async (): Promise<TTestimonialResponse> => {
-  return await http.get("/testimonial").then((res) => res.data);
+  return await http
+    .get("/testimonial", {
+      headers: {
+        "Cache-Control": "no-cache",
+      },
+    })
+    .then((res) => res.data);
 };
